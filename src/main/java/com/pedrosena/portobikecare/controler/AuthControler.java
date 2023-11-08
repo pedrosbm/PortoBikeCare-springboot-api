38ce584f76319f06d87ec2ff9de795afffbb7b3b
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2023-11-07 23:30:13 -0300

    login feito

--- src/main/java/com/pedrosena/portobikecare/controler/AuthControler.java
+++ src/main/java/com/pedrosena/portobikecare/controler/AuthControler.java
@@ -9,29 +9,40 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pedrosena.portobikecare.dao.ClienteDao;
 import com.pedrosena.portobikecare.dao.ClientePfDao;
 import com.pedrosena.portobikecare.dao.ClientePjDao;
+import com.pedrosena.portobikecare.dao.SenhaDao;
 import com.pedrosena.portobikecare.vo.ClientePfVo;
 import com.pedrosena.portobikecare.vo.ClientePjVo;
 import com.pedrosena.portobikecare.vo.ClienteVo;
 import com.pedrosena.portobikecare.vo.LoginVo;
+import com.pedrosena.portobikecare.vo.SenhaVo;
 
 @RestController
 @RequestMapping(value = "/Login")
 public class AuthControler {
 	ClienteDao cDao = new ClienteDao();
+	ClientePfDao pfDao = new ClientePfDao();
+	ClientePjDao pjDao = new ClientePjDao();
+	SenhaDao sDao = new SenhaDao();
 	
 	@PostMapping
 	public ResponseEntity<ClienteVo> login(@RequestBody LoginVo login){
 		
 		ClienteVo c = cDao.selectByEmail(login.getEmail());
+		SenhaVo s = sDao.selectById(c.getId());
 		
-		try {
-			if(c.getSenha().getSenha() == login.getSenha()) {
-				return ResponseEntity.ok(c);
+		if(s.getSenha().equals(login.getSenha())) {
+
+			ClientePfVo cPf = pfDao.selectById(c.getId());
+			ClientePjVo cPj = pjDao.selectById(c.getId());
+
+			if(cPf.getId() == 0){
+				return ResponseEntity.ok(cPj);
+			} else {
+				return ResponseEntity.ok(cPf);
 			}
-		} catch (Exception e) {
-			return ResponseEntity.ok(new ClienteVo());	
+			
+		}else {
+			return ResponseEntity.ok(new ClienteVo());
 		}
-		
-		return null;
 	}
 }
